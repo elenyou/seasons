e5883460601849bdd9e5edf7af27782c102ae4f3
elenyou <olenaovch@gmail.com>
2019-09-05 14:11:37 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
-    this.state = { 
-        lat: null,
-        errorMessage: ''
-    };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-        position => {
-            this.setState({
-                lat: position.coords.latitude
-            });
-        },
-        err => {
-            this.setState({
-                errorMessage: err.message
-            });
-        },
-      );
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      err => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
+    );
   }
 
   render() {
-    if(this.state.errorMessage && !this.state.lat) {
-        return <div> Error: {this.state.errorMessage} </div>;
-    } 
-    
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    }
+
     if (!this.state.errorMessage && this.state.lat) {
-        return <div> Latitude: {this.state.lat} </div>;
-    } 
-    
-    return <div>Loading!</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Loader message="Please accept location request"/>;
   }
 }
 
